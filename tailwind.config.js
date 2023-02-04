d0ca3bbc35b4349e3c12325a1a5ea3affcb9806f
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-02-03 23:42:53 -0300

    🔧 - customized theme added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      'light-green': "#B3DFB2",
+      'dark-green': "#425437",
+      'green': "#4E8228",
+      'brown': "#400A0A",
+      'lime': "#f7fee7",
+      'white': "#FFFFFF",
+      'hover-green': "#548C2B"
+    },
+
+    fontFamily: {
+      'Arial' : ["Arial", "Helvetica", "sans-serif"]
+    },
+
+    extend: {
+      backgroundImage: {
+        'footer-img': "url('../assets/footer-vegetables-credits-to-dose-juice.jpg')"
+      }
+    },
   },
   plugins: [],
 }
